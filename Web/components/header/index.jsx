07bd7a61d18f80ae0fe28cb90f44dee2b3658632
Jovaras Leonavicius <52199839+jowarnis@users.noreply.github.com>
2022-03-03 21:55:45 +0200

    working on tour singlePage, gallery implementation

--- Web/components/header/index.jsx
+++ Web/components/header/index.jsx
@@ -1,4 +1,4 @@
-import logo from "./Undiscovered/logo.png";
+import logo from "./logo.png";
 import Image from "next/image";
 import NavDesktop from "../navDesktop";
 import NavMobile from "../navMobile";
