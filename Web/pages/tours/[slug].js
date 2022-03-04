3a95d9fa1721f8c81d51699448ff5e6dab7644f9
Jovaras Leonavicius <52199839+jowarnis@users.noreply.github.com>
2022-03-04 23:22:55 +0200

    fixed deployment settings

--- Web/pages/tours/[slug].js
+++ Web/pages/tours/[slug].js
@@ -11,45 +11,46 @@ function urlFor(source) {
   return imageUrlBuilder(client).image(source);
 }
 const Tour = ({ tour }) => {
-  console.log(tour.page);
-  const tourContent = tour.page;
+  const tourContent = tour?.page;
   return (
     <>
-      <div className={styles.singleTourThemeWrapper}>
-        <div className={styles.singleTourWrapper}>
-          <section className={styles.tour}>
-            <FaMapSigns className={styles.icon} />
-            <h4 className={styles.singleTourTitle}>{tourContent.title}</h4>
-            <h6 className={styles.singleTourHTag}>{tourContent.hTag}</h6>
-            <ul className={styles.list}>
-              {tourContent.highLights.map((list, id) => {
-                return <li key={id}>{list}</li>;
-              })}
-            </ul>
-            <p>{tourContent.description}</p>
-          </section>
-          <section className={styles.gallery}>
-            <div className={styles.innerGallery}>
-              {tourContent.gallery?.slice(0, 4).map((galleryImg) => {
-                return (
-                  <div className={styles.imgContainer} key={galleryImg._key}>
-                    <img src={urlFor(galleryImg).format("webp")} />
-                    <div className={styles.imgOverlay}>
-                      <BsFillEyeFill className={styles.eyeIcon} />
+      {tourContent ? (
+        <div className={styles.singleTourThemeWrapper}>
+          <div className={styles.singleTourWrapper}>
+            <section className={styles.tour}>
+              <FaMapSigns className={styles.icon} />
+              <h4 className={styles.singleTourTitle}>{tourContent?.title}</h4>
+              <h6 className={styles.singleTourHTag}>{tourContent?.hTag}</h6>
+              <ul className={styles.list}>
+                {tourContent?.highLights?.map((list, id) => {
+                  return <li key={id}>{list}</li>;
+                })}
+              </ul>
+              <p>{tourContent?.description}</p>
+            </section>
+            <section className={styles.gallery}>
+              <div className={styles.innerGallery}>
+                {tourContent?.gallery?.slice(0, 4).map((galleryImg) => {
+                  return (
+                    <div className={styles.imgContainer} key={galleryImg._key}>
+                      <img src={urlFor(galleryImg).format("webp")} />
+                      <div className={styles.imgOverlay}>
+                        <BsFillEyeFill className={styles.eyeIcon} />
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
-            </div>
-            <div className={styles.galleryMessage}>
-              <div className={styles.iconWrapper}>
-                <FaThumbsUp className={styles.thumbsUpIcon} />
+                  );
+                })}
               </div>
-              <p>{tourContent.galleryMsg}</p>
-            </div>
-          </section>
+              <div className={styles.galleryMessage}>
+                <div className={styles.iconWrapper}>
+                  <FaThumbsUp className={styles.thumbsUpIcon} />
+                </div>
+                <p>{tourContent?.galleryMsg}</p>
+              </div>
+            </section>
+          </div>
         </div>
-      </div>
+      ) : null}
     </>
   );
 };
